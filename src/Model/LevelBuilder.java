a04d85a7415bc5eb2f0419b7a90f9e5ff290a983
Alexei Ermakov <ermakov@alexerox.ru>
2026-03-06 03:11:02 +0300

    Remake lelvels

--- src/Model/LevelBuilder.java
+++ src/Model/LevelBuilder.java
@@ -13,33 +13,33 @@ public class LevelBuilder {
         Level level = new Level(16, 10, "LEVEL_1");
 
         // BABA IS YOU
-        new TextBlock(new SubjectName("BABA"), level, new Position(0, 0));
-        new TextBlock(new IS(), level, new Position(1, 0));
-        new TextBlock(new YOU(), level, new Position(2, 0));
+        new TextBlock(new SubjectName("BABA"), level, new Position(2, 1));
+        new TextBlock(new IS(), level, new Position(3, 1));
+        new TextBlock(new YOU(), level, new Position(4, 1));
 
         // WALL IS STOP
-        new TextBlock(new SubjectName("WALL"), level, new Position(13, 0));
-        new TextBlock(new IS(), level, new Position(14, 0));
-        new TextBlock(new STOP(), level, new Position(15, 0));
+        new TextBlock(new SubjectName("WALL"), level, new Position(11, 1));
+        new TextBlock(new IS(), level, new Position(12, 1));
+        new TextBlock(new STOP(), level, new Position(13, 1));
 
         // ROCK IS PUSH
-        new TextBlock(new SubjectName("ROCK"), level, new Position(0, 9));
-        new TextBlock(new IS(), level, new Position(1, 9));
-        new TextBlock(new PUSH(), level, new Position(2, 9));
+        new TextBlock(new SubjectName("ROCK"), level, new Position(2, 8));
+        new TextBlock(new IS(), level, new Position(3, 8));
+        new TextBlock(new PUSH(), level, new Position(4, 8));
 
         // FLAG IS WIN
-        new TextBlock(new SubjectName("FLAG"), level, new Position(13, 9));
-        new TextBlock(new IS(), level, new Position(14, 9));
-        new TextBlock(new WIN(), level, new Position(15, 9));
+        new TextBlock(new SubjectName("FLAG"), level, new Position(11, 8));
+        new TextBlock(new IS(), level, new Position(12, 8));
+        new TextBlock(new WIN(), level, new Position(13, 8));
 
         // Стены
-        for(int i = 0; i < 16; i++){
-            new Subject("WALL", level, new Position(i, 1));
-            new Subject("WALL", level, new Position(i, 8));
+        for(int i = 2; i < 14; i++){
+            new Subject("WALL", level, new Position(i, 2));
+            new Subject("WALL", level, new Position(i, 7));
         }
 
         // Камни
-        for(int i = 2; i < 8; i++){
+        for(int i = 3; i < 7; i++){
             new Subject("ROCK", level, new Position(8, i));
         }
 
@@ -92,42 +92,42 @@ public class LevelBuilder {
         return level;
     }
     public static Level createLevel3() {
-        Level level = new Level(12, 10, "LEVEL_3");
+        Level level = new Level(13, 7, "LEVEL_3");
 
         // BABA IS YOU
-        new TextBlock(new SubjectName("BABA"), level, new Position(0, 0));
-        new TextBlock(new IS(), level, new Position(1, 0));
-        new TextBlock(new YOU(), level, new Position(2, 0));
+        new TextBlock(new SubjectName("BABA"), level, new Position(1, 1));
+        new TextBlock(new IS(), level, new Position(2, 1));
+        new TextBlock(new YOU(), level, new Position(3, 1));
 
         // WALL IS STOP
-        new TextBlock(new SubjectName("WALL"), level, new Position(9, 0));
-        new TextBlock(new IS(), level, new Position(10, 0));
-        new TextBlock(new STOP(), level, new Position(11, 0));
+        new TextBlock(new SubjectName("WALL"), level, new Position(5, 1));
+        new TextBlock(new IS(), level, new Position(6, 1));
+        new TextBlock(new STOP(), level, new Position(7, 1));
 
         // KEY IS PUSH
-        new TextBlock(new SubjectName("KEY"), level, new Position(4, 0));
-        new TextBlock(new IS(), level, new Position(5, 0));
-        new TextBlock(new PUSH(), level, new Position(6, 0));
+        new TextBlock(new SubjectName("KEY"), level, new Position(9, 1));
+        new TextBlock(new IS(), level, new Position(10, 1));
+        new TextBlock(new PUSH(), level, new Position(11, 1));
 
         // KEY IS OPEN
-        new TextBlock(new SubjectName("KEY"), level, new Position(0, 9));
-        new TextBlock(new IS(), level, new Position(1, 9));
-        new TextBlock(new OPEN(), level, new Position(2, 9));
+        new TextBlock(new SubjectName("KEY"), level, new Position(1, 5));
+        new TextBlock(new IS(), level, new Position(2, 5));
+        new TextBlock(new OPEN(), level, new Position(3, 5));
 
         // DOOR IS SHUT
-        new TextBlock(new SubjectName("DOOR"), level, new Position(4, 9));
-        new TextBlock(new IS(), level, new Position(5, 9));
-        new TextBlock(new SHUT(), level, new Position(6, 9));
+        new TextBlock(new SubjectName("DOOR"), level, new Position(5, 5));
+        new TextBlock(new IS(), level, new Position(6, 5));
+        new TextBlock(new SHUT(), level, new Position(7, 5));
 
         // FLAG IS WIN
-        new TextBlock(new SubjectName("FLAG"), level, new Position(9, 9));
-        new TextBlock(new IS(), level, new Position(10, 9));
-        new TextBlock(new WIN(), level, new Position(11, 9));
+        new TextBlock(new SubjectName("FLAG"), level, new Position(9, 5));
+        new TextBlock(new IS(), level, new Position(10, 5));
+        new TextBlock(new WIN(), level, new Position(11, 5));
 
-        new Subject("BABA", level,new Position(3, 6));
-        new Subject("KEY", level,new Position(5, 6));
-        new Subject("DOOR", level,new Position(7, 6));
-        new Subject("FLAG", level,new Position(9, 6));
+        new Subject("BABA", level,new Position(3, 3));
+        new Subject("KEY", level,new Position(5, 3));
+        new Subject("DOOR", level,new Position(7, 3));
+        new Subject("FLAG", level,new Position(9, 3));
 
         return level;
     }
